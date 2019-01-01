93236614753ebdb6120ef6c386e85cb89cc65673
asif mushtaq <asif.axi@gmail.com>
2019-01-01 13:15:41 +1100

    changed to the stateless function

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -2,29 +2,28 @@ import React from 'react'
 import BookShelf from './BookShelf'
 import { Link } from 'react-router-dom'
 
-class ListBooks extends React.Component {
-  shelfBooks = (bookShelf) => this.props.books.filter(book => book.shelf === bookShelf);
-    render () {
-        const { books, onShelfChanged } = this.props;
+const ListBooks = (props) => {
+  const shelfBooks = (bookShelf) => props.books.filter(book => book.shelf === bookShelf);
+  const { onShelfChanged } = props;
 
-        return (
-            <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                <BookShelf books={this.shelfBooks('currentlyReading')} onShelfChanged={onShelfChanged} title='Currently Reading' />
-                <BookShelf books={this.shelfBooks('wantToRead')} onShelfChanged={onShelfChanged} title='Want To Read' />
-                <BookShelf books={this.shelfBooks('read')} onShelfChanged={onShelfChanged} title='Read' />
-              </div>
-            </div>
-            <div className="open-search">
-              <Link to="/search">Add a book</Link>
-            </div>
-          </div>
-        );
-    }
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
+      </div>
+      <div className="list-books-content">
+        <div>
+          <BookShelf books={shelfBooks('currentlyReading')} onShelfChanged={onShelfChanged} title='Currently Reading' />
+          <BookShelf books={shelfBooks('wantToRead')} onShelfChanged={onShelfChanged} title='Want To Read' />
+          <BookShelf books={shelfBooks('read')} onShelfChanged={onShelfChanged} title='Read' />
+        </div>
+      </div>
+      <div className="open-search">
+        <Link to="/search">Add a book</Link>
+      </div>
+    </div>
+  );
+  
 }
 
 export default ListBooks;
