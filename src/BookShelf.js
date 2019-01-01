f6775b2bfe4e2baf65274ffa9ae4abcf1b56f151
asif mushtaq <asif.axi@gmail.com>
2019-01-01 13:24:40 +1100

    converted into the stateless function

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -2,30 +2,30 @@ import React from "react";
 import Book from "./Book";
 import PropTypes from "prop-types";
 
-class BookShelf extends React.Component {
-  static propTypes = {
-    books: PropTypes.array.isRequired,
-    title: PropTypes.string.isRequired
-  };
 
-  render() {
-    const { books, title, onShelfChanged } = this.props;
+const BookShelf = (props) => {
+  const { books, title, onShelfChanged } = props;
    
-    return (
-      <div>
-        <h2 className="bookshelf-title">{title}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {books.map(book => (
-              <li key={book.id}>
-                <Book book={book} onShelfChanged={onShelfChanged}/>
-              </li>
-            ))}
-          </ol>
-        </div>
+  return (
+    <div>
+      <h2 className="bookshelf-title">{title}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {books.map(book => (
+            <li key={book.id}>
+              <Book book={book} onShelfChanged={onShelfChanged}/>
+            </li>
+          ))}
+        </ol>
       </div>
-    );
-  }
+    </div>
+  );
+}
+
+
+BookShelf.propTypes = {
+  books: PropTypes.array.isRequired,
+  title: PropTypes.string.isRequired
 }
 
 export default BookShelf;
